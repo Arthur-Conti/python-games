45a5ed04588cf9a88e79a1e5e3da40bf93b30bf9
Arthur Conti Gomes da Silva <agomes@ccmrp.local>
2023-05-18 16:16:14 -0300

    Atualizado sistema de validação da aposta do jogador e adiciono novo jogo: Dados

--- dados.py
+++ dados.py
@@ -1,23 +1,22 @@
-from termcolor import colored
-import os
 import random
-import json
+import os
+from termcolor import colored
 
 # Cabeçalho de boas vindas
-def bem_vindo_carros():
+def bem_vindo_dados():
     os.system("cls")
     print("")
-    print(colored("BEM VINDO A CORRIDA DE CARROS", "blue", attrs=["bold"]), end="\n\n")
-    menu_escolha_carros()
+    print(colored("BEM VINDO A CORRIDA DE DADOS", "blue", attrs=["bold"]), end="\n\n")
+    menu_escolha_dados()
     
 # Menu de escolha principal
-def menu_escolha_carros(): 
+def menu_escolha_dados(): 
     print("[1] Começar a jogar")
     print("[2] Como o jogo funciona")
-    escolha_carros()
+    escolha_dados()
     
 # Verificação da escolha 
-def escolha_carros():
+def escolha_dados():
     escolha_1 = int(input("Opção: "))
     
     if(escolha_1 == 1):
@@ -29,16 +28,16 @@ def escolha_carros():
     else:
         os.system("cls")
         print(colored("Favor escolher uma opção valida!", "red", attrs=["bold"]))
-        menu_escolha_carros()
+        menu_escolha_dados()
   
 # Manual de funcionamento do jogo      
 def funcionamento():
     os.system("cls")
-    print(colored("""O jogo se baseia em sorte, nele você irá correr contra outros dois carros quem tiver a maior velocidade ganha, lembresse de não apostar 
-tudo que tem, visto que é um jogo de sorte""", attrs=["bold"]), end="\n\n")
+    print(colored("""O jogo se baseia em sorte, nele você irá rolar uma dado de 6 lados e escolher um lado, se o dado cair no lado escolhido vc ganha, se não, perde tudo.
+Lembresse de não apostar tudo que tem, visto que é um jogo de sorte""", attrs=["bold"]), end="\n\n")
     input(colored("Aperte ENTER para voltar ao menu principal", "red", attrs=["bold"]))
     os.system("cls")
-    menu_escolha_carros()
+    menu_escolha_dados()
     
 # Cadostro do usuário
 def cadastro():
@@ -52,7 +51,6 @@ def cadastro():
         tem_dinheiro(saldo)
     #inserir_no_json(nome, saldo)
     
-# Verifica se o jogador não está apostando um valor maior do que ele tem
 def tem_dinheiro(saldo):
     aposta = float(input("Insira o valor que deseja apostar: "))
     if(aposta > saldo):
@@ -60,31 +58,24 @@ def tem_dinheiro(saldo):
         print("Você não tem tanto dinheiro assim, insira um valor menor ou igual ao seu saldo atual de", saldo, "reais")
         tem_dinheiro(saldo)
     else:
-        jogo_carros(saldo, aposta)
+        jogo_dados(saldo, aposta)
     
-# Define velocidade aleatoria para cada carro e valida vencedor
-def jogo_carros(saldo, aposta):
-    velocidade_jogador = round(random.randrange(1, 101))
-    velocidade1 = round(random.randrange(1, 101))
-    velocidade2 = round(random.randrange(1, 101))
+def jogo_dados(saldo, aposta):
+    os.system("cls")
+    chute = int(input("Chute um lado [1 a 6]: "))
     
-    if(velocidade_jogador > velocidade1 and velocidade_jogador > velocidade2):
-        aposta = aposta + (aposta * 2)
+    lados = round(random.randrange(1, 7))
+    
+    if(chute == lados):
         saldo = saldo + aposta
-        os.system("cls")
-        print("Você venceu!!!")
-        print(f"O valor do seu saldo agora é: {saldo}")
-        print(f"A velocidade do seu carro era {velocidade_jogador} km/h, a do segundo carro era {velocidade1} km/h, e a do terceiro carro era {velocidade2} km/h")
-        continuar_jogo(saldo)
+        print("Você acertou!!! Seu saldo agora é", saldo, "reais")
+        continuar_jogo(saldo, aposta)
     else:
-        os.system("cls")
         saldo = saldo - aposta
-        print("Você perdeu :(")
-        print(f"O valor do seu saldo agora é: {saldo}")
-        print(f"A velocidade do seu carro era {velocidade_jogador} km/h, a do segundo carro era {velocidade1} km/h, e a do terceiro carro era {velocidade2} km/h")
-        continuar_jogo(saldo)
-        
-# Oferece a opção de continuar jogando ao jogador
+        print("Você errou!!! O dado caiu no lado", lados) 
+        print("Seu saldo agora é", saldo, "reais")
+        continuar_jogo(saldo, aposta)
+
 def continuar_jogo(saldo):
     escolha_continuar = input("Deseja continuar jogando? ")
     escolha_continuar = escolha_continuar.lower()
@@ -94,7 +85,7 @@ def continuar_jogo(saldo):
         escolha_rejogar = input("Deseja começar novamente?")
         if(escolha_rejogar == 'sim'):
             os.system("cls")
-            bem_vindo_carros()
+            bem_vindo_dados()
         elif(escolha_rejogar == "nao"):
             os.system("cls")
             print("Obrigado por jogar")
@@ -104,9 +95,5 @@ def continuar_jogo(saldo):
     elif(escolha_continuar == "nao"):
         os.system("cls")
         print("Obrigado por jogar")
-        
-#def inserir_no_json(nome, saldo):
-#    dif_dados = {"nome": nome, "saldo": saldo}
 
-bem_vindo_carros()
-    
+bem_vindo_dados()
